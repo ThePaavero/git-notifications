86cd4b2a4b6f1ad11eda2ebbc7f08e3931dd74d9
pekka <pekka.siiriainen@activeark.com>
2017-03-02 14:39:38 +0200

    More debugging

--- node-client/index.js
+++ node-client/index.js
@@ -30,6 +30,9 @@ const buildMessage = (event) => {
 
 socket.on('data', event => {
   console.log('Event received, showing notification')
+  console.log('--------------------------------')
+  console.log(event)
+  console.log('--------------------------------')
   notifier.notify({
     'title': 'New git event',
     'message': buildMessage(event),
